c1542a7dbe562d34c49a7c4883984a0519f34d74
PlatinumMan <fistonrwanda@gmail.com>
2023-04-25 18:06:28 +0200

    feat: add logging with winston and morgan

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
+import { morganMiddleware, systemLogs } from './utils/Logger.js';
 
 const app = express();
 
@@ -13,6 +14,8 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(morganMiddleware);
 
 app.get('/api/v1/test', (req, res) => {
   res.json({ message: 'test api' });
@@ -25,5 +28,8 @@ app.listen(PORT, () => {
     `${chalk.green('✓')} Server running in ${chalk.yellow.yellow(
       process.env.NODE_ENV
     )} mode on port ${chalk.blue.bold(process.env.PORT)}`
+  );
+  systemLogs.info(
+    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
   );
 });
