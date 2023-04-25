4103bab9d9f4b9f40c39d96cd92efa06ef16c863
PlatinumMan <fistonrwanda@gmail.com>
2023-04-25 13:47:39 +0200

    chore: setup basic express backend api

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import chalk from 'chalk';
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/api/v1/test', (req, res) => {
+  res.json({ message: 'test api' });
+});
+
+const PORT = process.env.PORT || 2001;
+
+app.listen(PORT, () => {
+  console.log(
+    `${chalk.green('✓')} Server running in ${chalk.yellow.yellow(
+      process.env.NODE_ENV
+    )} mode on port ${chalk.blue.bold(process.env.PORT)}`
+  );
+});
